d0ce2e4a3f5e8bdfd49a169c5c13ed182a15976e
Sairaj <153702108+SairajPP@users.noreply.github.com>
2026-05-12 22:08:27 +0530

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-def login_user(username, password):
-    # Intentional bad code for testing!
-    hardcoded_secret = "my_super_secret_password_123"
-    if password == hardcoded_secret:
-        print("Logged in!")
-        whi True:
-            pass # Intentional infinite
