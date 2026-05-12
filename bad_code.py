b757e97ee8d379aed7df90b95f40a9cb973583eb
Sairaj <153702108+SairajPP@users.noreply.github.com>
2026-05-12 22:12:29 +0530

    Create bad_code.py

---
+++ bad_code.py
@@ -0,0 +1,7 @@
+def login_user(username, password):
+    # Intentional bad code for testing!
+    hardcoded_secret = "my_super_secret_password_123"
+    if password == hardcoded_secret:
+        print("Logged in!")
+        while True:
+            pass # Intentional infinite loop
