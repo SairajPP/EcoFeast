36c071ba4c61267aedc135f4696dd78509df7f0f
Sairaj <153702108+SairajPP@users.noreply.github.com>
2026-05-13 21:42:22 +0530

    Delete fresstest.py

--- fresstest.py
+++
@@ -1,8 +0,0 @@
-def login_user(username, password):
-    # Intentional bad code for testing!
-    hardcoded_secret = "my_super_secret_password_123"
-    if password == hardcoded_secret:
-        print("Logged in!")
-        while True:
-            pass # Intentional 
-            loop\
